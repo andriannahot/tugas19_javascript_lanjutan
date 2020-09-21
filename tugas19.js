28d37922e91f220f10f0dadb4f5ee4d1790de977
andriannahot <64577548+andriannahot@users.noreply.github.com>
2020-09-21 11:49:15 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,25 @@
+var ulang = 'semangat...'
+
+var hasil2 = ulang.repeat(5)
+
+console.log(hasil2)
+
+
+var ulang1 = '*****'
+
+var hasil = ulang1.repeat(1)
+var hasil2 = ulang1.repeat(4)
+var hasil3 = ulang1.repeat(8)
+var hasil4 = ulang1.repeat(12)
+var hasil5 = ulang1.repeat(8)
+var hasil6 = ulang1.repeat(4)
+var hasil7 = ulang1.repeat(1)
+
+console.log(hasil)
+console.log(hasil2)
+console.log(hasil3)
+console.log(hasil4)
+console.log(hasil5)
+console.log(hasil6)
+console.log(hasil7)
+
